feb424a729dfa7491a20aebb95dbf1cd4d5f0ef3
zhenya194 <zhenya.pro1753@gmail.com>
2026-03-07 12:47:14 +0200

    Update comments for tg bot

--- apps/tgbot/handlers/common.py
+++ apps/tgbot/handlers/common.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025-2026 Andrey Kotlyar <guitar0.app@gmail.com>
+# SPDX-FileCopyrightText: 2026 Andrey Kotlyar <guitar0.app@gmail.com>
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
